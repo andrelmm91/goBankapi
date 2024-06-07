1d9b6313143bb00d9e8a16790bd099dd70cbaf87
andrelmm91 <andre.lmm91@gmail.com>
2024-06-07 21:29:54 +0200

    unit test transfer DB

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,89 @@
+package db
+
+import (
+	"context"
+	"simplebank/util"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        util.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+
+	// testing using package Testify
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, transfer.Amount, arg.Amount)
+	require.Equal(t, transfer.FromAccountID, arg.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, arg.ToAccountID)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.FromAccountID)
+	require.NotZero(t, transfer.ToAccountID)
+	require.NotZero(t, transfer.Amount)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+// test CreateTransfer
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	createRandomTransfer(t, account1, account2)
+}
+
+// test GetTransfer
+func TestGetTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	transfer1 := createRandomTransfer(t, account1, account2)
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+
+	// testing using package Testify
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+
+// test ListTransfers
+func TestListTransfers(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t, account1, account2)
+	}
+
+	arg := ListTransfersParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Limit:         5,
+		Offset:        5,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+
+	// testing using package Testify
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+	}
+}
