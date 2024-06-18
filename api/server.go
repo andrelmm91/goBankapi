2d6b93c6fa0f111264919aba7bdaad37483dfa9e
andrelmm91 <110781476+andrelmm91@users.noreply.github.com>
2024-06-18 20:06:39 +0000

    transfer API endpoint

--- api/server.go
+++ api/server.go
@@ -20,6 +20,7 @@ func NewServer(store db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+	router.POST("/transfers", server.createTransfer)
 
 	// server
 	server.router = router
