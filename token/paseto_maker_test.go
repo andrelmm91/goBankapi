79b4d475e5d6b77989014cfd61fc217a391ec41b
andrelmm91 <110781476+andrelmm91@users.noreply.github.com>
2024-06-24 11:00:06 +0000

    PasetoToken OAuth and unit tests

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,61 @@
+package token
+
+import (
+	"simplebank/util"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, payload, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestPasetoKeyLengthMaker(t *testing.T) {
+	// KeyLength should be equal to 32
+	_, err := NewPasetoMaker(util.RandomString(31))
+	require.Error(t, err)
+}
+
+func TestExpiredInvalidPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, payload, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	// Expired
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+
+	// Invalid
+	tokenInvalid := util.RandomString(32)
+	payload, err = maker.VerifyToken(tokenInvalid)
+	require.Nil(t, payload)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+}
